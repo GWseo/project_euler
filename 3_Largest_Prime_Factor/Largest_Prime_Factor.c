3008787ce253daca25f89eb0f27620d7facd1b76
GWseo <seogw@hotmail.com>
2017-03-23 02:28:58 +0900

    3_Largest_Prime_Factor - try sieve reveser way

--- 3_Largest_Prime_Factor/Largest_Prime_Factor.c
+++ 3_Largest_Prime_Factor/Largest_Prime_Factor.c
@@ -7,7 +7,8 @@
 #include <stdbool.h>
 
 /*
-
+    First, try sieve of eratosthenes but, Test Case #4, #5 time out
+    Second, run sieve reverse   -> #4, #5 time out
 */
 
 int isPrime(long n){
@@ -31,13 +32,14 @@ int main(){
         long n; 
 		long i = 0, largestP = 0;
         scanf("%ld",&n);
-        largestP = n;
-		for( i = 2 ; i < n/2+1 ; i++){	//we don't need to check more then half of itself
+        largestP = 1;
+		for( i = n/2+1 ; i > largestP ; i--){	//we don't need to check more then half of itself
 			if( n % i == 0) {
                 if(isPrime(i))
                     largestP = i;
 			}
 		}
+        if(largestP == 1) largestP = n;
         printf("%ld\n",largestP);
     }
     return 0;
